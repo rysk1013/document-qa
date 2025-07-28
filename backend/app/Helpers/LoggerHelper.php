dd2da13cd0a800a788c8411381de6c7f4cb6dc8f
rysk(mac) <swpa0x33d@gmail.com>
2025-07-28 16:26:18 +0900

    update: log

--- backend/app/Helpers/LoggerHelper.php
+++ backend/app/Helpers/LoggerHelper.php
@@ -3,6 +3,13 @@
 use Illuminate\Support\Facades\Log;
 
 if (!function_exists('logInfo')) {
+    /**
+     * output info level log
+     *
+     * @param  string $message
+     * @param  array  $context
+     * @return void
+     */
     function logInfo(string $message, array $context = []): void
     {
         Log::channel('daily')->info($message, $context);
@@ -10,6 +17,13 @@ if (!function_exists('logInfo')) {
 }
 
 if (!function_exists('logError')) {
+    /**
+     * output error level log
+     *
+     * @param  string $message
+     * @param  array  $context
+     * @return void
+     */
     function logError(string $message, array $context = []): void
     {
         Log::channel('daily')->error($message, $context);
